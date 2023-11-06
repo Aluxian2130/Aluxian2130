c01d20756cf96a491219e47c0703e99fe513bc49
OsmanS1 <148401842+OsmanS1@users.noreply.github.com>
2023-11-05 17:48:36 -0700

    Update SellerUnitTest.java

--- src/org/ecp/test/SellerUnitTest.java
+++ src/org/ecp/test/SellerUnitTest.java
@@ -1,13 +1,24 @@
+package org.ecp.test;
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SellerTest {
+import java.util.ArrayList;
+
+import org.ecp.items.Product;
+import org.ecp.people.Seller;
+
+public class SellerUnitTest {
 
     @Test
     public void testSetProductList() {
         Seller seller = new Seller();
-        ArrayList<Product> productList = new ArrayList<>();
-        Product product1 = new Product("Product 1", 10.0);
+        ArrayList<Product> productList = new ArrayList<Product>();
+        Product product1 = new Product("Product 1", 10.0, 3);
         productList.add(product1);
         seller.setProductList(productList);
         assertEquals("Product list should be set", productList, seller.getProductList());
@@ -16,32 +27,21 @@ public class SellerTest {
     @Test
     public void testAddProduct() {
         Seller seller = new Seller();
-        Product product = new Product("Test Product", 20.0);
+        Product product = new Product("Test Product", 20.0, 4);
         // You might need to simulate user input for the product details (name, price, quantity, description)
         // Mocking user input is beyond the scope of this test, so it's not shown here.
-        seller.addProduct(product);
-        assertTrue("Seller should have the added product", seller.getProductList().contains(product));
+        seller.addToProductList(product);
+        assertTrue("Seller should have the added product: ", seller.getProductList().contains(product));
     }
 
     @Test
     public void testRemoveProduct() {
         Seller seller = new Seller();
-        Product product = new Product("Product 1", 10.0);
-        seller.addProduct(product);
+        Product product = new Product("Product 1", 10.0, 5);
+        seller.addToProductList(product);
         seller.removeProduct(product);
-        assertFalse("Seller should not have the removed product", seller.getProductList().contains(product));
+        assertFalse("Seller should not have the removed product: ", seller.getProductList().contains(product));
     }
 
-    @Test
-    public void testUpdateProduct() {
-        Seller seller = new Seller();
-        Product product = new Product("Product 1", 10.0);
-        seller.addProduct(product);
-        // You might need to simulate user input for the field to be modified and the new value
-        // Mocking user input is beyond the scope of this test, so it's not shown here.
-        seller.updateProduct(product);
-        // Assert that the product's field is updated as expected
-        // For instance, you can use assertEquals to compare the new value with the expected value
-        // Example: assertEquals("Product name should be updated", "New Name", product.getName());
-    }
+    
 }
