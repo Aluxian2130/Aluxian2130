d236f11e10b82f769fd593415329a1f5590dc531
Soloveiyko <148399117+Soloveiyko@users.noreply.github.com>
2023-11-30 20:05:19 -0700

    Add DriverGUI

---
+++ src/org/ecp/test/Driver_Files/DriverGUI.java
@@ -0,0 +1,12 @@
+package org.ecp.test.Driver_Files;
+import org.ecp.gui.files.HomePage;
+
+public class DriverGUI {
+	public static void main(String[] args)
+	{
+		HomePage home = new HomePage("HomePage GUI");    
+	}
+}
+
+
+
