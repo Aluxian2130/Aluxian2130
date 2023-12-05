56b890ace336dbd28753cc575ef2c6e1bfbe5131
Soloveiyko <148399117+Soloveiyko@users.noreply.github.com>
2023-12-05 09:03:00 -0700

    Drivergui

--- src/org/ecp/test/Driver_Files/DriverGUI.java
+++ src/org/ecp/test/Driver_Files/DriverGUI.java
@@ -61,91 +61,67 @@ public class DriverGUI {
     	OnlineMaps om1 = new OnlineMaps();
     	
     	//To test CustomerPage
-<<<<<<< HEAD
-    			Customer c1 = new Customer();
-    			c1.setEmail("Wayne@email");
-    			c1.setUsername("Wayne");
-    			c1.setAccountType("Customer");
-    			c1.setAddress("5678 W Elm St");
-    			c1.setPassword("password_100");
-    			c1.setAccountBalance(3590);
-    			Admin.getUserList().add(c1);
+    	Customer c1 = new Customer();
+    	c1.setEmail("Wayne@email");
+    	c1.setUsername("Wayne");
+    	c1.setAccountType("Customer");
+    	c1.setAddress("5678 W Elm St");
+    	c1.setPassword("password_100");
+    	c1.setAccountBalance(3590);
+    	Admin.getUserList().add(c1);
     			
-    			Customer c2 = new Customer();
-    			c2.setEmail("Sorla@email");
-    			c2.setUsername("Sorla");
-    			c2.setAccountType("Customer");
-    			c2.setAddress("1234 E Willow Dr");
-    			c2.setPassword("password_70");
-    			c2.setAccountBalance(3590);
-    			Admin.getUserList().add(c2);
-    			//CustomerPage cp = new CustomerPage(c1.getEmail(), c1.getUsername(), c1.getPassword(), c1.getAccountType(), 2000.0);
+    	Customer c2 = new Customer();
+    	c2.setEmail("Sorla@email");
+    	c2.setUsername("Sorla");
+    	c2.setAccountType("Customer");
+    	c2.setAddress("1234 E Willow Dr");
+    	c2.setPassword("password_70");
+    	c2.setAccountBalance(3590);
+    	Admin.getUserList().add(c2);
+    	//CustomerPage cp = new CustomerPage(c1.getEmail(), c1.getUsername(), c1.getPassword(), c1.getAccountType(), 2000.0);
     			
-    			Order o1 = new Order();
-    			o1.setCustomerAddress(c1.getAddress());
-    			o1.setCustomerName(c1.getUsername());
-    			o1.setDeliveryDate(1205);
-    			o1.setStatus("Ordered");
-    			o1.addProductNames(prod_A.getName());
-    			o1.addProductNames(prod_B.getName());
-    			Admin.getDeliveryList().add(o1);
+    	Order o1 = new Order();
+    	o1.setCustomerAddress(c1.getAddress());
+    	o1.setCustomerName(c1.getUsername());
+    	o1.setDeliveryDate(1205);
+    	o1.setStatus("Ordered");
+    	o1.addProductNames(prod_A.getName());
+    	o1.addProductNames(prod_B.getName());
+    	Admin.getDeliveryList().add(o1);
     			
-    			Order o2 = new Order();
-    			o2.setCustomerAddress(c2.getAddress());
-    			o2.setCustomerName(c2.getUsername());
-    			o2.setDeliveryDate(1205);
-    			o2.setStatus("Ordered");
-    			o2.addProductNames(prod_B.getName());
-    			o2.addProductNames(prod_C.getName());
-    			Admin.getDeliveryList().add(o2);
-    			
+    	Order o2 = new Order();
+    	o2.setCustomerAddress(c2.getAddress());
+    	o2.setCustomerName(c2.getUsername());
+    	o2.setDeliveryDate(1205);
+    	o2.setStatus("Ordered");
+    	o2.addProductNames(prod_B.getName());
+    	o2.addProductNames(prod_C.getName());
+    	Admin.getDeliveryList().add(o2);
     			
     			
-    			//To test sellerPage
-    			Seller s1 = new Seller();
-    			s1.setEmail("Robin@email");
-    			s1.setUsername("Robin");
-    			s1.setAccountType("Seller");
-    			s1.setPassword("password_90");
-    			s1.setAccountBalance(3590);
-    			Admin.getUserList().add(s1);
-    			//SellerPage p = new SellerPage(s1);
     			
-    			//To test sellerPage
-    			Driver d1 = new Driver();
-    			d1.setEmail("Lisa@email");
-    			d1.setUsername("Lisa");
-    			d1.setAccountType("Driver");
-    			d1.setPassword("password_80");
-    			d1.setAccountBalance(3590);
-    			Admin.getUserList().add(d1);
-    		
-    			//TO TEST AS A WHOLE
-    			GUI home = new GUI("E commerce Platform");      
-=======
-		Customer c1 = new Customer();
-		c1.setEmail("Wayne@email");
-		c1.setUsername("Wayne");
-		c1.setAccountType("Customer");
-		c1.setAddress(null);
-		c1.setPassword("blegh123");
-		c1.setAccountBalance(3590);
-		Admin.getUserList().add(c1);
-		//CustomerPage cp = new CustomerPage(c1.getEmail(), c1.getUsername(), c1.getPassword(), c1.getAccountType(), 2000.0);
-		
-		
-		//To test sellerPage
-		Seller s1 = new Seller();
-		s1.setEmail("Robin@email");
-		s1.setUsername("Robin");
-		s1.setAccountType("Seller");
-		s1.setAccountBalance(3590);
-		Admin.getUserList().add(s1);
-		//SellerPage p = new SellerPage(s1);
-	
-		//TO TEST AS A WHOLE
-		GUI home = new GUI("E commerce Platform");    
->>>>>>> 28b6c0d1bbe6c55bf055ee314d3abbe6ee82bac5
+    	//To test sellerPage
+    	Seller s1 = new Seller();
+    	s1.setEmail("Robin@email");
+    	s1.setUsername("Robin");
+    	s1.setAccountType("Seller");
+    	s1.setPassword("password_90");
+    	s1.setAccountBalance(3590);
+    	Admin.getUserList().add(s1);
+    	//SellerPage p = new SellerPage(s1);
+    			
+    	//To test driverPage
+    	Driver d1 = new Driver();
+    	d1.setEmail("Lisa@email");
+    	d1.setUsername("Lisa");
+    	d1.setAccountType("Driver");
+    	d1.setPassword("password_80");
+    	d1.setAccountBalance(3590);
+    	Admin.getUserList().add(d1);
+    	
+    	//TO TEST AS A WHOLE
+    	GUI home = new GUI("E commerce Platform");      
+
 		
 	}
 }
