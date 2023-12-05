d493174e3e1a7b84afd837e13b92ef3b0e7aab50
Alex <rsantos4@arizona.edu>
2023-12-04 17:19:13 -0700

    SellerPage updated

--- src/org/ecp/gui/files/SellerPage.java
+++ src/org/ecp/gui/files/SellerPage.java
@@ -1,46 +1,91 @@
 package org.ecp.gui.files;
-
-import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import org.ecp.people.User;
+import javax.swing.JOptionPane;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+import org.ecp.items.Product;
+import org.ecp.people.Seller;
 
 public class SellerPage {
-    JFrame frame = new JFrame();
-    JLabel title = new JLabel("E-Commerce Platform");
-    JLabel header = new JLabel("Welcome Seller!");
-    JButton productListBtn = new JButton("See Product List");
-    JButton accountBalanceBtn = new JButton("Account Balance");
+    private JFrame frame = new JFrame();
+    private JLabel title = new JLabel("E-Commerce Platform");
+    private JLabel header = new JLabel("Welcome Seller!");
+    private JButton addProductBtn = new JButton("Add Product");
+    private JButton removeProductBtn = new JButton("Remove Product");
+    private JButton viewProductListBtn = new JButton("View Product List");
 
-    public SellerPage(User user_A) {
+    private List<Product> productList = new ArrayList<>();
+
+    public SellerPage(Seller s_1) {
         title.setBounds(30, 10, 300, 30);
         title.setFont(new Font("serif", Font.BOLD, 25));
-        title.setForeground(new java.awt.Color(211, 84, 0));
-        header.setBounds(40, 60, 200, 35); // Moved down the welcome message
+        title.setForeground(new Color(211, 84, 0));
+        header.setBounds(40, 60, 200, 35);
         header.setFont(new Font("serif", Font.BOLD, 15));
-        header.setForeground(new java.awt.Color(225, 246, 76)); // lime
+        header.setForeground(new Color(225, 246, 76));
 
-        // Set positions and sizes for buttons
-        productListBtn.setBounds(150, 110, 200, 30);
-        accountBalanceBtn.setBounds(150, 160, 200, 30);
+        addProductBtn.setBounds(150, 110, 200, 30);
+        removeProductBtn.setBounds(150, 160, 200, 30);
+        viewProductListBtn.setBounds(150, 210, 200, 30);
+
+        addProductBtn.addActionListener(this::handleAddProduct);
+        removeProductBtn.addActionListener(this::handleRemoveProduct);
+        viewProductListBtn.addActionListener(this::handleViewProductList);
 
         frame.add(header);
         frame.add(title);
-        frame.add(productListBtn);
-        frame.add(accountBalanceBtn);
+        frame.add(addProductBtn);
+        frame.add(removeProductBtn);
+        frame.add(viewProductListBtn);
 
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new java.awt.Color(40, 55, 71));
+        frame.getContentPane().setBackground(new Color(40, 55, 71));
         frame.setSize(500, 500);
         frame.setLayout(null);
         frame.setVisible(true);
     }
-	
-    // This is here for debugging and coding purposes. Remove for final deliverable.
-   // public static void main(String[] args) {
-        // Instantiate SellerPage and see the GUI
-   //     new SellerPage();
-   // }
+
+    private void handleAddProduct(ActionEvent e) {
+        String name = JOptionPane.showInputDialog(frame, "Enter Product Name:");
+        String description = JOptionPane.showInputDialog(frame, "Enter Product Description:");
+        double price = Double.parseDouble(JOptionPane.showInputDialog(frame, "Enter Product Price:"));
+        int quantity = Integer.parseInt(JOptionPane.showInputDialog(frame, "Enter Product Quantity:"));
+
+        if (name != null && description != null && !name.isEmpty() && !description.isEmpty()) {
+            Product newProduct = new Product(name, price, quantity);
+            newProduct.setDescription(description);
+            productList.add(newProduct);
+            JOptionPane.showMessageDialog(frame, "Product added successfully!");
+        } else {
+            JOptionPane.showMessageDialog(frame, "Please provide valid information!");
+        }
+    }
+
+    private void handleRemoveProduct(ActionEvent e) {
+        String name = JOptionPane.showInputDialog(frame, "Enter Product Name to Remove:");
+
+        for (Product product : productList) {
+            if (product.getName().equalsIgnoreCase(name)) {
+                productList.remove(product);
+                JOptionPane.showMessageDialog(frame, "Product removed successfully!");
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(frame, "Product not found!");
+    }
+
+    private void handleViewProductList(ActionEvent e) {
+        StringBuilder products = new StringBuilder("Product List:\n");
+        for (Product product : productList) {
+            products.append(product.getName()).append(" - $").append(product.getPrice()).append("\n");
+        }
+        JOptionPane.showMessageDialog(frame, products.toString());
+    }
 }
+//    public static void main(String[] args) {
+//        new SellerPage();
+//    }
+//}
